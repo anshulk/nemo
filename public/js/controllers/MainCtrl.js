1b61109e0a43ede6885093b2a06ae5d0bf8143a5
ashish <ashish@adurcup.com>
2016-12-08 23:22:53 +0530

    initial responsive

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -23,7 +23,7 @@ angular.module('MainCtrl', ['jkAngularRatingStars']).controller('MainController'
   $scope.LetsPlay = function(e){
     var movie_id = e.target.attributes.movie.value;
     var status = e.target.attributes.status.value;
-    var visible_card = e.toElement.offsetParent.parentNode;
+    var visible_card = e.toElement.offsetParent.parentNode.parentNode;
     var visible_card_index = visible_card.getAttribute("card-index");
     var arriving_card = document.querySelectorAll("[card-index='"+(parseInt(visible_card_index)+1)+"']");
     var onhold_card = document.querySelectorAll("[card-index='"+(parseInt(visible_card_index)+2)+"']");
@@ -36,7 +36,7 @@ angular.module('MainCtrl', ['jkAngularRatingStars']).controller('MainController'
         angular.element(arriving_card).removeClass('arriving').addClass('visible');
         angular.element(onhold_card).removeClass('onhold').addClass('arriving');
         angular.element(load_card).removeClass('hide').addClass('onhold');
-      },600);
+      },400);
     }else{
       angular.element(visible_card).removeClass('visible').addClass('ignored');
       setTimeout(function(){
@@ -44,7 +44,7 @@ angular.module('MainCtrl', ['jkAngularRatingStars']).controller('MainController'
         angular.element(arriving_card).removeClass('arriving').addClass('visible');
         angular.element(onhold_card).removeClass('onhold').addClass('arriving');
         angular.element(load_card).removeClass('hide').addClass('onhold');
-      },600);
+      },400);
     }
   }
 });
