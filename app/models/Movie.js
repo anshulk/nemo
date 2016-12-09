d1b88eb65de25e9844c89036a86abee83025dc49
Anshul Kushwaha <anshulkushwaha1@gmail.com>
2016-12-09 11:28:59 +0530

    [FEATURE] Added Genres to movie object

--- app/models/Movie.js
+++ app/models/Movie.js
@@ -3,6 +3,11 @@ var mdb        = require('moviedb')(config.key);
 var _          = require('lodash');
 module.exports = {
   insertCast: function(movie, cb){
+    var genres = {};
+    movie.genre_ids.forEach(function(id){
+      genres[id] = config.genres[id];
+    });
+    movie.genres = genres;
     mdb.movieCredits({id: movie.id}, function(err, credits){
       console.log("Credits are : ", credits);
       if(credits && credits.cast) movie.cast = credits.cast;
